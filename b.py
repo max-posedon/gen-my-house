cab62ca63a40a9a7cdca4425995c38afc9038ec9
Maksim Melnikau <maxposedon@gmail.com>
2018-09-13 01:06:36 +0300

    named walls

--- b.py
+++ b.py
@@ -60,39 +60,43 @@ class House:
 			self.altitude = altitude
 			self.width = width
 			self.depth = depth
-			self.walls = []
+			self.walls = {}
 			
-			self.wall_left = self.add_d_wall((-1,1), -1+thickness/width, thickness)
-			self.wall_right = self.add_d_wall((-1,1),  1-thickness/width, thickness)
-			self.wall_back = self.add_w2_wall(self.wall_left, self.wall_right,  1-thickness/depth, thickness)
-			self.wall_front = self.add_w2_wall(self.wall_left, self.wall_right, -1+thickness/depth, thickness)
+			self.add_d_wall((-1,1), -1+thickness/width, thickness, "left")
+			self.add_d_wall((-1,1),  1-thickness/width, thickness, "right")
+			self.add_w2_wall("left", "right",  1-thickness/depth, thickness, "back")
+			self.add_w2_wall("left", "right", -1+thickness/depth, thickness, "front")
 			
-		def add_w_wall(self, relative_width, relative_depth, thickness):
+		def add_w_wall(self, relative_width, relative_depth, thickness, name):
 			size = (self.width*(relative_width[1]-relative_width[0])/2, thickness, self.height)
 			location = (self.width*(relative_width[1]+relative_width[0])/4, self.depth/2*relative_depth, 0)
 			wall = House.Floor.Wall(size, location)
-			self.walls.append(wall)
+			self.walls[name] = wall
 			return wall
 			
-		def add_d_wall(self, relative_depth, relative_width, thickness):
+		def add_d_wall(self, relative_depth, relative_width, thickness, name):
 			size = (thickness, self.depth*(relative_depth[1]-relative_depth[0])/2, self.height)
 			location = (self.width/2*relative_width, self.depth*(relative_depth[1]+relative_depth[0])/4, 0)
 			wall = House.Floor.Wall(size, location)
-			self.walls.append(wall)
+			self.walls[name] = wall
 			return wall
 			
-		def add_w2_wall(self, left_wall, right_wall, relative_depth, thickness):
+		def add_w2_wall(self, left, right, relative_depth, thickness, name):
+			left_wall = self.walls[left]
+			right_wall = self.walls[right]
 			size = (right_wall.bound_left-left_wall.bound_right, thickness, self.height)
 			location = ((right_wall.bound_left+left_wall.bound_right)/2, self.depth/2*relative_depth, 0)
 			wall = House.Floor.Wall(size, location)
-			self.walls.append(wall)
+			self.walls[name] = wall
 			return wall
 		
-		def add_d2_wall(self, front_wall, back_wall, relative_width, thickness):
+		def add_d2_wall(self, front, back, relative_width, thickness, name):
+			front_wall = self.walls[front]
+			back_wall = self.walls[back]
 			size = (thickness, back_wall.bound_front-front_wall.bound_back, self.height)
 			location = (self.width/2*relative_width, (back_wall.bound_front+front_wall.bound_back)/2, 0)
 			wall = House.Floor.Wall(size, location)
-			self.walls.append(wall)
+			self.walls[name] = wall
 			return wall
 		
 	def __init__(self, width, depth):
@@ -188,7 +192,7 @@ class BlenderHouse:
 			e = bpy_add_cube(size=(1+B_E, 1+B_E, 1+B_E), location=(0,0, floor.altitude+floor.height/2))
 			bpy_obj_minus_obj(f, e)
 			
-			for wall in floor.walls:
+			for name,wall in floor.walls.items():
 				w = bpy_add_cube(
 					size=wall.size,
 					location=(wall.location[0], wall.location[1], floor.altitude + wall.size[2]/2)
@@ -223,25 +227,25 @@ f2 = house.add_floor(height=3, thickness=0.6)
 
 IWT = 0.3
 
-f1d0 = f1.add_d2_wall(f1.wall_front, f1.wall_back, 0, IWT)
-f1w1 = f1.add_w2_wall(f1.wall_left, f1d0, 0.33, IWT)
-f1w2 = f1.add_w2_wall(f1d0, f1.wall_right,0.33, IWT)
-f1d3 = f1.add_d2_wall(f1.wall_front, f1w1, -0.5, IWT)
-f1.add_w2_wall(f1d0, f1.wall_right, -0.33, IWT)
-f1.add_w2_wall(f1.wall_left, f1d3, -0.33, IWT)
+f1.add_d2_wall("front", "back", 0, IWT, "f1d0")
+f1.add_w2_wall("left", "f1d0", 0.33, IWT, "f1w1")
+f1.add_w2_wall("f1d0", "right", 0.33, IWT, "f1w2")
+f1.add_d2_wall("front", "f1w1", -0.5, IWT, "f1d3")
+f1.add_w2_wall("f1d0", "right", -0.33, IWT, "f1w4")
+f1.add_w2_wall("left", "f1d3", -0.33, IWT, "f1w5")
 
 IWT = 0.3
 
-f2w0 = f2.add_w2_wall(f2.wall_left, f2.wall_right, 0.33, IWT)
-f2d1 = f2.add_d2_wall(f2w0, f2.wall_back, 0, IWT)
-f2d2 = f2.add_d_wall((-0.68,0.3), -0.5, IWT)
+f2.add_w2_wall("left", "right", 0.33, IWT, "f2w0")
+f2.add_d2_wall("f2w0", "back", 0, IWT, "f2d1")
+f2.add_d_wall((-0.68, 0.3), -0.5, IWT, "f2d2")
 
-f2.add_w2_wall(f2.wall_left, f2d2, 0.1, IWT)
-f2.add_w2_wall(f2.wall_left, f2d2, -0.33, IWT)
-f2.add_w2_wall(f2.wall_left, f2d2, -0.66, IWT)
+f2.add_w2_wall("left", "f2d2", 0.1, IWT, "f2w3")
+f2.add_w2_wall("left", "f2d2", -0.33, IWT, "f2w4")
+f2.add_w2_wall("left", "f2d2", -0.66, IWT, "f2w5")
 
-f2.add_d_wall((-1,-0.33), 0, IWT)
-f2.add_w_wall((0,1), -0.33, IWT)
+f2.add_d_wall((-1,-0.33), 0, IWT, "f2d6")
+f2.add_w_wall((0,1), -0.33, IWT, "f2w7")
 
 # render ground
 bpy_add_cube(name='ground', size=(30,50,B_E), location=(0,0,-B_E/2))
