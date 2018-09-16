ced6b60a3cc1a1fc309ff35b34b1b9243ed19aa2
Maksim Melnikau <maxposedon@gmail.com>
2018-09-16 03:02:19 +0300

    doors & doors

--- b.py
+++ b.py
@@ -40,19 +40,57 @@ class House:
 			def bound_front(self):
 				return self.location[1]-self.size[1]/2
 			
-			def add_w_hole(self, relative_width, width, height, base_height):
-				size = (width, self.size[1], height)
-				location = (self.size[0]/2*relative_width, 0, base_height+(height-self.size[2])/2)
+			def add_hole(self, size, location):
 				hole = House.Floor.Wall.Hole(size, location)
 				self.holes.append(hole)
 				return hole
 			
+			def add_w_hole(self, relative_width, width, height, base_height):
+				size = (width, self.size[1], height)
+				location = (self.size[0]/2*relative_width, 0, base_height+(height-self.size[2])/2)
+				return self.add_hole(size, location)
+			
 			def add_d_hole(self, relative_depth, depth, height, base_height):
 				size = (self.size[0], depth, height)
 				location = (0, self.size[1]/2*relative_depth, base_height+(height-self.size[2])/2)
-				hole = House.Floor.Wall.Hole(size, location)
-				self.holes.append(hole)
-				return hole
+				return self.add_hole(size, location)
+				
+			def add_w2_hole(self, gap, pos, width, height, base_height):
+				size = (width, self.size[1], height)
+				location = (
+					(self.size[0]/2 - gap - width/2)*pos,
+					0,
+					base_height+(height-self.size[2])/2
+					)
+				return self.add_hole(size, location)
+				
+			def add_d2_hole(self, gap, pos, depth, height, base_height):
+				size = (self.size[0], depth, height)
+				location = (
+					0, 
+					(self.size[1]/2 - gap - depth/2)*pos, 
+					base_height+(height-self.size[2])/2
+					)
+				return self.add_hole(size, location)
+			
+			def add_w3_hole(self, gap_wall, gap, pos, width, height, base_height):
+				size = (width, self.size[1], height)
+				if pos > 0:
+					location_0 = gap_wall.bound_left - self.location[0] - gap - width/2
+				else:
+					location_0 = gap_wall.bound_right - self.location[0] + gap + width/2
+				location = (location_0, 0, base_height+(height-self.size[2])/2)
+				return self.add_hole(size, location)
+			
+			def add_d3_hole(self, gap_wall, gap, pos, depth, height, base_height):
+				size = (self.size[0], depth, height)
+				if pos > 0:
+					location_1 = gap_wall.bound_front - self.location[1] - gap - depth/2
+				else:
+					location_1 = gap_wall.bound_back - self.location[1] + gap + depth/2
+				location = (0, location_1, base_height+(height-self.size[2])/2)
+				return self.add_hole(size, location)
+			
 	
 		def __init__(self, height, thickness, altitude, width, depth):
 			self.height = height
@@ -67,37 +105,34 @@ class House:
 			self.add_w2_wall("left", "right",  1-thickness/depth, thickness, "back")
 			self.add_w2_wall("left", "right", -1+thickness/depth, thickness, "front")
 			
-		def add_w_wall(self, relative_width, relative_depth, thickness, name):
-			size = (self.width*(relative_width[1]-relative_width[0])/2, thickness, self.height)
-			location = (self.width*(relative_width[1]+relative_width[0])/4, self.depth/2*relative_depth, 0)
+		def add_wall(self, size, location, name):
 			wall = House.Floor.Wall(size, location)
 			self.walls[name] = wall
 			return wall
 			
+		def add_w_wall(self, relative_width, relative_depth, thickness, name):
+			size = (self.width*(relative_width[1]-relative_width[0])/2, thickness, self.height)
+			location = (self.width*(relative_width[1]+relative_width[0])/4, self.depth/2*relative_depth, 0)
+			return self.add_wall(size, location, name)
+			
 		def add_d_wall(self, relative_depth, relative_width, thickness, name):
 			size = (thickness, self.depth*(relative_depth[1]-relative_depth[0])/2, self.height)
 			location = (self.width/2*relative_width, self.depth*(relative_depth[1]+relative_depth[0])/4, 0)
-			wall = House.Floor.Wall(size, location)
-			self.walls[name] = wall
-			return wall
+			return self.add_wall(size, location, name)
 			
 		def add_w2_wall(self, left, right, relative_depth, thickness, name):
 			left_wall = self.walls[left]
 			right_wall = self.walls[right]
 			size = (right_wall.bound_left-left_wall.bound_right, thickness, self.height)
 			location = ((right_wall.bound_left+left_wall.bound_right)/2, self.depth/2*relative_depth, 0)
-			wall = House.Floor.Wall(size, location)
-			self.walls[name] = wall
-			return wall
+			return self.add_wall(size, location, name)
 		
 		def add_d2_wall(self, front, back, relative_width, thickness, name):
 			front_wall = self.walls[front]
 			back_wall = self.walls[back]
 			size = (thickness, back_wall.bound_front-front_wall.bound_back, self.height)
 			location = (self.width/2*relative_width, (back_wall.bound_front+front_wall.bound_back)/2, 0)
-			wall = House.Floor.Wall(size, location)
-			self.walls[name] = wall
-			return wall
+			return self.add_wall(size, location, name)
 		
 	def __init__(self, width, depth):
 		self.width = width
@@ -249,12 +284,19 @@ f1.walls['back'].add_w_hole(0.2, 1, 1, 1)
 f1.walls['right'].add_d_hole(0, 2, 1, 1)
 f1.walls['right'].add_d_hole(0.6, 2, 1, 1)
 
-f1.walls['f1d3'].add_d_hole(-0.8, 1, 2, 0)
-f1.walls['f1d3'].add_d_hole(0.8, 1.5, 2, 0)
+f1.walls['f1d3'].add_d3_hole(f1.walls['front'], 0.2, -1, 1, 2, 0)
+f1.walls['f1d3'].add_d3_hole(f1.walls['f1w1'], 0, 1, 1.5, 2, 0)
+f1.walls['f1d0'].add_d3_hole(f1.walls['f1w1'], 0, 1, 1.5, 2, 0)
+f1.walls['f1d0'].add_d3_hole(f1.walls['f1w4'], 0.2, 1, 1, 2, 0)
+
+f1.walls['f1w1'].add_w3_hole(f1.walls['left'], 0.2, -1, 1, 2, 0)
+f1.walls['f1w4'].add_w3_hole(f1.walls['f1d0'], 0.2, -1, 1, 2, 0)
+f1.walls['f1w2'].add_w3_hole(f1.walls['f1d0'], 0, -1, 1, 2, 0)
+f1.walls['f1w2'].add_w3_hole(f1.walls['right'], 0.4, 1, 3, 1.5, 0.5)
 
 f2.add_w2_wall("left", "right", 0.33, IWT, "f2w0")
 f2.add_d2_wall("f2w0", "back", 0, IWT, "f2d1")
-f2.add_d_wall((-0.68, 0.3), -0.5, IWT, "f2d2")
+f2.add_d_wall((-0.66, 0.33), -0.5, IWT, "f2d2")
 
 f2.add_w2_wall("left", "f2d2", 0.1, IWT, "f2w3")
 f2.add_w2_wall("left", "f2d2", -0.33, IWT, "f2w4")
@@ -262,6 +304,13 @@ f2.add_w2_wall("left", "f2d2", -0.66, IWT, "f2w5")
 
 f2.add_d_wall((-1,-0.33), 0, IWT, "f2d6")
 f2.add_w_wall((0,1), -0.33, IWT, "f2w7")
+
+f2.walls['f2w0'].add_w3_hole(f2.walls['f2d1'], 0.2, 1, 1, 2, 0)
+f2.walls['f2w0'].add_w3_hole(f2.walls['f2d1'], 0.2, -1, 1, 2, 0)
+f2.walls['f2w7'].add_w3_hole(f2.walls['f2d6'], 0.2, -1, 1, 2, 0)
+f2.walls['f2d2'].add_d3_hole(f2.walls['f2w0'], 0.2, 1, 1, 2, 0)
+f2.walls['f2d2'].add_d3_hole(f2.walls['f2w5'], 0.2, -1, 1, 2, 0)
+f2.walls['f2w3'].add_w3_hole(f2.walls['f2d2'], 0.2, 1, 1, 2, 0)
 
 # render ground
 bpy_add_cube(name='ground', size=(27,54,B_E), location=(0,0,-B_E/2))
